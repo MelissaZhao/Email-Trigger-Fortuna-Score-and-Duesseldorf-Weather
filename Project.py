84d42707482e5b8d831f8b1162b5d720a65d9204
Melissa Zhao <58794164+MelissaZhao@users.noreply.github.com>
2020-12-10 18:33:03 +0100

    Update Project.py

--- Project.py
+++ Project.py
@@ -18,9 +18,6 @@ from email.mime.image import MIMEImage
 from email import encoders
 
 
-
-
-
 # This project aims to send the regular Email to football fan of Fortuna-Düsseldorf club about the football score of Fortuna and the weather in Düsseldorf on that day
 
 
